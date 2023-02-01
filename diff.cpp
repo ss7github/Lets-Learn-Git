a1750db7bc84ab7f17f8b3e20162636a83ee5313
SS7 <ssilakari2k@gmail.com>
2023-02-01 11:10:11 +0530

    added implementation for diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int diff=0;
+    int a,b;
+    cin>>a>>b;
+
+    diff = a-b;
+    cout<<"Difference is "<<diff<<endl;
+    
+
+    return 0;
+
+}
