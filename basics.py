984ca32a837682014c38552207b9b05d493d3e64
ss7github <68783435+ss7github@users.noreply.github.com>
2023-02-06 13:58:21 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -12,5 +12,19 @@ else:
     print('Zero')
     
 #2.odd or even
+num=int(input())
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
+
+#3. sum of first n natural numbers
+n = int(input())
+sum=0
+for i in range(n +1):
+    sum+=i
+print (sum)
+
+#slicing in python ----- imp and easy function
     
     
