3322951096d7597000de795e367a87d2156c366e
ss7github <68783435+ss7github@users.noreply.github.com>
2023-02-09 00:23:26 +0530

    Update basics.cpp

--- basics.cpp
+++ basics.cpp
@@ -1,1 +1,12 @@
 
+// Prime or Composite
+int isPrime(int N){
+        if(N==1) return 0;
+
+        for(int i=2;i*i<=N;i++){
+            if(N%i==0) return 0;
+        }
+        return 1;
+    }
+
+//
