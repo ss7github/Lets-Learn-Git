96c6dac96391a00c147852c6dd662143bde29e08
ss7github <68783435+ss7github@users.noreply.github.com>
2023-02-19 20:49:58 +0530

    Update GFG.py

--- GFG.py
+++ GFG.py
@@ -1,4 +1,16 @@
 #--------------------------------------------------------------------------------------------------------------
+# 2 Arrays Equal or Not
+class Solution:
+    #Function to check if two arrays are equal or not.
+    def check(self,A,B,N):
+        
+        A = sorted(A)
+        B = sorted(B)
+        for i in range(N):
+            if A[i] != B[i]:
+                return False
+        return True
+#--------------------------------------------------------------------------------------------------------------
 # Is it Fibonacci
 class Solution():
     def solve(self, N, K, GeekNum):
