9b38aa02d076ab54baa558442d1c21cc91476936
ss7github <68783435+ss7github@users.noreply.github.com>
2023-02-21 19:56:27 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -4,6 +4,25 @@
 using namespace std;
 
 int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    
+	    int a[n];
+	    for(int i=0; i<n; i++){
+	        cin>>a[i];
+	    }
+	    
+	    cout<< <<endl;
+	}
+	return 0;
+//
+#include <iostream>
+using namespace std;
+
+int main() {
 	int t,n;
 	cin>>t;
 	while(t--){
