98a0b66406e6160f629bb2f1e6717bb3510eb3ee
ss7github <68783435+ss7github@users.noreply.github.com>
2023-02-22 19:48:32 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -1,5 +1,31 @@
 //-----------------------------------------------------------------------------------
 //
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    
+	    int a[n];
+	    for(int i=0; i<n; i++) cin>>a[i];
+	    
+	    sort (a, a+n);
+	    double sum=0;
+	    for(int i=k; i<n-k; i++){
+	        sum+=a[i];
+	    }
+	    double avg=sum/(n-2*k);
+	    
+	    cout<<fixed<<setprecision(6)<<avg<<endl;
+	}
+	return 0;
+}
+
+//
 #include <iostream>
 using namespace std;
 
