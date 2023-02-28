9e78f3cde12e755fe38aa6f75c3ee3ed7c19c8e3
ss7github <68783435+ss7github@users.noreply.github.com>
2023-02-28 20:37:51 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -1,4 +1,15 @@
 //-----------------------------------------------------------------------------------
+//boilerplate
+int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int a[n];
+	    for(int i=0; i<n; i++){
+	        cin>>a[i];
+	    }
+	}
 //
 #include <bits/stdc++.h>
 using namespace std;
