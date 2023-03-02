fce11f1297c651e88c784a625c10db9f58c7935d
ss7github <68783435+ss7github@users.noreply.github.com>
2023-03-02 20:49:11 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -23,6 +23,27 @@ int t;
 	    }
 	}
 //-----------------------
+//#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    string s;
+        cin>>s;
+        if(n==2) cout<<s<<endl;
+        else{
+            sort(s.begin(), s.end());
+            cout<<s<<endl;
+        }
+	}
+	return 0;
+}
+
+//------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
 
