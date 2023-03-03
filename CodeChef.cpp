b60c6122959620e69a06796a597e8322628d449b
ss7github <68783435+ss7github@users.noreply.github.com>
2023-03-03 23:15:08 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -43,6 +43,33 @@ int main() {
 	return 0;
 }
 
+//--------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b, cnt=0;
+        cin>>a>>b;
+        
+        if(a==b){
+            cout<<0<<endl;
+        }else{
+            while(b%a != 0){
+                a /= 2;
+                cnt++;
+            }
+            while(b != a){
+                a *= 2;
+                cnt++;
+            }
+            cout<<cnt<<endl;
+        }
+	}
+	return 0;
+}
 //------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
