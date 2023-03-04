0ae3f50c29c5a838c0848f1b599b4fb8aa9afd5f
ss7github <68783435+ss7github@users.noreply.github.com>
2023-03-04 21:03:45 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -43,6 +43,32 @@ int main() {
 	return 0;
 }
 
+//--------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int arr[x];
+	    map<int,int>mp;
+	   for(int i=0;i<x;i++){
+	       cin>>arr[i];
+	       mp[arr[i]]++;
+	   }
+	   int sum=0;
+	   for(auto i : mp){
+	       sum =max(sum,(i.first+i.second-1));
+	   }
+	   cout<<sum<<endl;
+	   
+	}
+	return 0;
+}
 //--------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
