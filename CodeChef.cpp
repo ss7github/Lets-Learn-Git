a5559864169d6f4bb29acf7572a2fb856427ae2c
ss7github <68783435+ss7github@users.noreply.github.com>
2023-03-05 23:58:23 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -229,3 +229,20 @@ int main() {
 	return 0;
 }
 //-----------------------------------------------------------------------------------
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    l = list(map(int, input().split()))
+    answer, i = "YES", 0
+    
+    while i < n-1:
+        if l[i] > l[i+1]:
+            if l[i] + l[i+1] > x:
+                answer = "NO"
+                break
+            else:
+                t = l[i]
+                l[i] = l[i+1]
+                l[i+1] = t
+        i += 1
+        
+    print(answer)
