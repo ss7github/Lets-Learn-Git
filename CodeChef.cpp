7c83874b1b6120117e1fee4b1fec4cbad8601a06
ss7github <68783435+ss7github@users.noreply.github.com>
2023-03-08 15:16:25 +0530

    Update CodeChef.cpp

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -43,6 +43,22 @@ int main() {
 	return 0;
 }
 
+//-------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(k==0) cout<<n<<endl;
+	    else cout<<n%k<<endl;
+	}
+	return 0;
+}
+
 //----------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
