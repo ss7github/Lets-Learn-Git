81998c5011becb649c3a716e1f559781a9021b66
ss7github <68783435+ss7github@users.noreply.github.com>
2023-03-22 21:18:10 +0530

    Update CodeChef.py

--- CodeChef.py
+++ CodeChef.py
@@ -11,7 +11,7 @@
 #-----------------------------
 
 #-----------------------------
-# Codechef Problem
+#Codechef Problem
 T = int(input())
 for i in range(T):
     A,B,A1,B1,A2,B2=map(int,input().split())
